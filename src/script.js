71609d7b59d7dd10e19f81f14c24e365436efe38
Galaktikos <40399277+Galaktikos@users.noreply.github.com>
2020-04-20 13:21:25 -0400

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,51 @@
+let home = document.getElementById("home");
+let header = document.getElementById("header");
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		  
+	  	while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		  
+	  	if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+
+	return false;
+}
+
+function moveTo(section) {
+	header.style.animation = "fade-out-u 1s ease-out";
+
+	setTimeout(function () {
+		header.style.left = "100vw";
+	}, 995);
+	
+	home.style.animation = "blip-out .4s ease";
+
+	setTimeout(function () {
+		home.style.left = "100vw";
+
+		document.getElementById(section).style.left = "35%";
+		document.getElementById(section).style.animation = "grow-in 1s ease";
+	}, 390);
+
+	alert(getCookie(section));
+
+	if (getCookie(section)) {
+		alert("e");
+	}
+}
+
+function start(section) {
+	alert(getCookie(section));
+	document.cookie = section + "=1;path=/";
+	alert(getCookie(section));
+}
