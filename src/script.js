9b77e09e6bfa90910fb2b728cd8d5bbbd9b46bbf
Galaktikos <40399277+Galaktikos@users.noreply.github.com>
2020-05-04 23:56:50 -0400

    Update

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 let home = document.getElementById("home");
 let header = document.getElementById("header");
+let step;
 
 function moveTo(section) {
 	header.style.animation = "fade-out-u 1s ease-out";
@@ -20,8 +21,6 @@ function moveTo(section) {
 			document.getElementById(section + "Confirm").style.opacity = "1";
 		}, 300);
 	}, 390);
-
-	document.getElementById(section + "Continue").style.opacity = "0";
 }
 
 function start(section) {
@@ -30,6 +29,43 @@ function start(section) {
 	document.getElementById(section).style.height = "70%";
 	document.getElementById(section).style.left = "17.5%";
 	document.getElementById(section).style.top = "15%";
+	document.getElementById(section + "Learn").style.left = "5%";
+	document.getElementById(section + "Learn").style.transition = "opacity 1s";
+	document.getElementById(section + "Learn").style.opacity = 1;
+	document.getElementById(section + "Step").innerHTML = "Base Equation";
+	document.getElementById(section + "EquationStep1").style.opacity = 1;
+	document.getElementById(section + "EquationStep1").style.bottom = "50%";
+	document.getElementById(section + "EquationStep1").style.color = "rgb(0, 150, 220)";
+	step = 1;
+}
+
+function next(section, final) {
+	if (step - 1 == final) {
+		location.reload();
+	}
+
+	step++;
+	document.getElementById(section + "Step").innerHTML = "Step " + (step - 1);
+	document.getElementById(section + "EquationStep" + step).style.opacity = 1;
+	document.getElementById(section + "EquationStep" + step).style.color = "rgb(0, 150, 220)";
+	document.getElementById(section + "EquationStep" + (step - 1)).style.color = "rgb(20, 20, 20)";
+
+	if (step > 2)
+		document.getElementById(section + "Step" + (step - 2)).style.opacity = 0;
+
+	document.getElementById(section + "Step" + (step - 1)).style.opacity = 1;
+
+	for (let i = 0; i < step; i++) {
+		document.getElementById(section + "EquationStep" + (i + 1)).style.bottom = (10 * (step - i)) + "%";
+
+		if (step - i > 4)
+			document.getElementById(section + "EquationStep" + (i + 1)).style.opacity = 0;
+	}
+
+	if (step - 1 == final) {
+		document.getElementById(section + "Next").innerHTML = "Finish";
+		document.getElementById(section + "EquationStep" + step).style.color = "rgb(0, 209, 0)";
+	}
 }
 
 function back(section) {
